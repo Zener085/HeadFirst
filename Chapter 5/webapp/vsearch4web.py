63823ccdb8a5d1cb0d534a2c8b4b99fda561d2b2
Zener_085 <Zener085@users.noreply.github.com>
2021-02-01 08:26:12 +0300

    Update vsearch4web.py

--- Chapter 5/webapp/vsearch4web.py
+++ Chapter 5/webapp/vsearch4web.py
@@ -17,7 +17,7 @@ def do_search() -> str:
 
 
 @app.route('/entry')
-def entry_page() -> 'html':
+def entry_page() -> html:
     return render_template('entry.html', name='name',
                            the_title='Welcome to search4letters on the web!')
 
