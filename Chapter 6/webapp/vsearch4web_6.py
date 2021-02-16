e02119921ed50c47b338720a81bbcefe1736cd56
Zener_085 <57841939+Zener085@users.noreply.github.com>
2021-02-16 09:08:56 +0300

    adding viewlog.log

--- Chapter 6/webapp/vsearch4web_6.py
+++ Chapter 6/webapp/vsearch4web_6.py
@@ -31,5 +31,12 @@ def entry_page() -> html:
                            the_title='Welcome to search4letters on the web!')
 
 
+@app.route('/viewlog')
+def view_the_log() -> str:
+    with open('vsearch.log') as log:
+        contests = log.read()
+        return contests
+
+
 if __name__ == '__main__':
     app.run(debug=True)
